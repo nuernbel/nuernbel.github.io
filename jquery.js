4867eeaba19621b1d9eb7d8c6a52be76b7ce0c0b
Maddy Welch <MaddyWelch@maddys-mbp.eduroam.student>
2018-03-19 15:07:36 +0000

    Can now press 'enter' instead of pressing add
    on the index, also removed highlight on the input box

--- jquery.js
+++ jquery.js
@@ -1,5 +1,11 @@
 $("#button").click(function(event) {
   var text = $("#ingredient").val();
   $("#list").append('<li>' + text + '</li>');
-  
+});
+
+$(document).keypress(function(e) {
+    if(e.which == 13) {
+      var text = $("#ingredient").val();
+      $("#list").append('<li>' + text + '</li>');
+    }
 });
